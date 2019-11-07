1e2ab10dd0c82b6dab52782d247405da384b6159
Josephb1234 <josephburke62@hotmail.com>
2019-11-07 22:30:34 +0000

    Added in change for character validation.

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -19,6 +19,17 @@ class Dec2Hex
                 System.exit(1);
             }
         }
+        else (args.length == 0)
+        {
+            try
+            {
+                Arg1 = Integer.parseInt(args[0]);
+            }
+            catch (NumberFormatException e)
+            {
+                System.err.println("You must enter a value!");
+            }
+        }
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
